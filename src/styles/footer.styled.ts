c2991a1202ef670c37e7232d752119aee30001b2
junyeokk <cjh4302@gmail.com>
2024-05-02 01:20:51 +0900

    feat: footer 컴포넌트 구현 (#3)

---
+++ src/styles/footer.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  background-color: #b0b0b0;
+  color: white;
+  text-align: center;
+  width: 100%;
+  padding: 0;
+  font-size: 0.75rem;
+  border-radius: 10px 10px 0 0;
+
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+`;
