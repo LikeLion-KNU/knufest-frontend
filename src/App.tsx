3e8d53b69fa1ccf7d0715908b0d6de03f51857a1
toothlessdev <toothless042@gmail.com>
2024-05-10 22:11:47 +0900

    feature: 페이지 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from "react";
-import HomePage from "./components/HomePage";
+import { Provider } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+
+import { MainLayout } from "@/common/layout/MainLayout";
+
+import HomePage from "@/pages/HomePage";
+
 import { GlobalStyle } from "./app.styled";
+import BoothDetailPage from "./pages/BoothDetailPage";
+import BoothListPage from "./pages/BoothListPage";
+import ContributorPage from "./pages/ContributorPage";
+import SpecialGuestPage from "./pages/SpecialGuestPage";
+import TimeTablePage from "./pages/TimeTablePage";
+import { store } from "@/store/store";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <HomePage />
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <Provider store={store}>
+                <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                        <Route index path="/" element={<HomePage />} />
+                        <Route path="/timetable" element={<TimeTablePage />}></Route>
+                        <Route path="/booth" element={<BoothListPage />}></Route>
+                        <Route path="/booth/:id" element={<BoothDetailPage />}></Route>
+                        <Route path="/guests" element={<SpecialGuestPage />}></Route>
+                        <Route path="/contributors" element={<ContributorPage />}></Route>
+                    </Route>
+                </Routes>
+            </Provider>
+        </>
+    );
 };
 
 export default App;
