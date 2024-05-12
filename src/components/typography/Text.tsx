20ae63e9fba83fb88e6edb368d9bd01d9738af78
junyeokk <cjh4302@gmail.com>
2024-05-12 11:07:34 +0900

    feat: 텍스트 색 추가 (#5)

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export interface IText {
     size: "xs" | "s" | "m" | "l" | "xl" | string;
     weight: "bold" | "normal";
+    variant?: "darkpurple" | "purple" | "white" | "lightpurple";
 }
 
 export const Text = styled.span<IText>`
@@ -22,6 +23,19 @@ export const Text = styled.span<IText>`
                 return "56px";
             default:
                 return props.size;
+        }
+    }};
+
+    color: ${(props) => {
+        switch (props.variant) {
+            case "darkpurple":
+                return "#5d5a88";
+            case "purple":
+                return "#767494";
+            case "lightpurple":
+                return "#adabc3";
+            case "white":
+                return "#ffffff";
         }
     }};
 `;
