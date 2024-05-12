dc80624368009f015d0a0efe9e90480389ed3314
junyeokk <cjh4302@gmail.com>
2024-05-12 11:02:25 +0900

    feat: 푸터 컴포넌트 공통 컴포넌트 요소들로 변경 (#5)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 
-import { FooterContainer } from "@/components/layout/Footer.styled";
+import { FooterContainer, Logo, EventName, LegalLinks, TopBorder, LegalText } from "@/components/layout/Footer.styled";
+import { Text } from "@/components/typography/Text";
+
+import logo from "@/assets/hapuruna.png";
 
 const Footer: React.FC = () => {
     return (
         <FooterContainer>
-            <p>© 2024 LikeLion KNU. All rights reserved.</p>
+            <TopBorder />
+            <Logo src={logo} alt="Event Logo" />
+            <EventName>
+                <Text size="l" weight="normal" variant="purple">
+                    2024 경북대학교 대동제
+                </Text>
+            </EventName>
+            <LegalLinks>
+                <LegalText>
+                    <Text size="s" weight="normal" variant="lightpurple">
+                        © 2024 LikeLion
+                    </Text>
+                    <Text size="xs" weight="normal" variant="purple">
+                        &nbsp;|&nbsp;
+                    </Text>
+                    <Text size="s" weight="normal" variant="lightpurple">
+                        All Rights Reserved
+                    </Text>
+                </LegalText>
+                <LegalText>
+                    <Text size="xs" weight="normal" variant="lightpurple">
+                        Terms and Conditions
+                    </Text>
+                    <Text size="xs" weight="normal" variant="purple">
+                        &nbsp;|&nbsp;
+                    </Text>
+                    <Text size="xs" weight="normal" variant="lightpurple">
+                        Privacy Policy
+                    </Text>
+                </LegalText>
+            </LegalLinks>
         </FooterContainer>
     );
 };
