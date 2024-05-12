769f4f94ee9cbb2eb47d80d41203071cd0953a74
junyeokk <cjh4302@gmail.com>
2024-05-12 11:03:44 +0900

    feat: 폰트 globalstyle 설정 (#5)

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "MesloLGS NF Bold";
+    src: url("./assets/MesloLGS-NF-Bold.ttf") format("truetype");
+  }
+
   body {
     margin: 0;
     padding: 0;
