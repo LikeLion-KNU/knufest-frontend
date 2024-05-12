af5cea5876633320f04e5cece2051f903c237972
junyeokk <cjh4302@gmail.com>
2024-05-12 11:07:15 +0900

    feat: 푸터 디자인 (#5)

--- src/components/layout/Footer.styled.ts
+++ src/components/layout/Footer.styled.ts
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
-    background-color: #b0b0b0;
-    color: white;
     text-align: center;
-    width: 100%;
-    padding: 0;
-    font-size: 0.75rem;
-    border-radius: 10px 10px 0 0;
+    padding: 20px 0;
 
-    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+    bottom: 0;
+`;
+
+export const TopBorder = styled.div`
+    height: 1px;
+    background-color: #d0d0d0;
+    width: 70%;
+    margin: 0 auto 20px;
+`;
+
+export const Logo = styled.img`
+    width: 100px;
+    height: auto;
+`;
+
+export const EventName = styled.h3`
+    margin: 30px 0;
+`;
+
+export const LegalLinks = styled.div``;
+
+export const LegalText = styled.p`
+    margin: 0 0 10px 0;
 `;
