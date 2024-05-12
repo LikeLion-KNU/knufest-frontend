97d9fd2b2d34a159cde7fb2c58d6304c48eda1ad
Hyoeunkh <lhe4528@gmail.com>
2024-05-12 15:01:40 +0900

    refactor: 기존코드 lint 및 코드 포매팅 resolve #7

--- src/components/display/Booth.styled.ts
+++ src/components/display/Booth.styled.ts
@@ -1,56 +1,56 @@
-import styled from "styled-components";
 import { motion } from "framer-motion";
+import styled from "styled-components";
 
 export const BoothList = styled(motion.li)`
-	height: 40px;
-  padding: 20px 25px;
-	margin: 12px 0;
-	border-radius: 25px;
-	background-color: #E9E9FB;
-	align-content: center;
+    height: 40px;
+    padding: 20px 25px;
+    margin: 12px 0;
+    border-radius: 25px;
+    background-color: #e9e9fb;
+    align-content: center;
 `;
 
 export const BoothLink = styled.a`
-	position: relative;
-  text-decoration: none;
-	display: flex;
-	align-items: center;
+    position: relative;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
 
-	span {
-		position: absolute;
-		left: 50px;
-	}
+    span {
+        position: absolute;
+        left: 50px;
+    }
 `;
 
 export const Index = styled.div`
-	position: absolute;
-	left: 0;
-	width: 30px;
-	height: 30px;
-	border-radius: 10px;
-	align-content: center;
-	text-align: center;
-	font-weight: bolder;
-	font-size: 20px;
+    position: absolute;
+    left: 0;
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    align-content: center;
+    text-align: center;
+    font-weight: bolder;
+    font-size: 20px;
 
-	color: #fff;
-	background-color: #9874FF;
+    color: #fff;
+    background-color: #9874ff;
 `;
 
 export const Heart = styled.div`
-	position: absolute;
-	right: 0;
-	width: 40px;
-	height: 40px;
-	border-radius: 50%;
-	background-color: #fff;
+    position: absolute;
+    right: 0;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #fff;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	padding: 5px;
-	font-size: 12px;
-	line-height: 8px;
-	color: #9874ff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+    font-size: 12px;
+    line-height: 8px;
+    color: #9874ff;
 `;
--- src/components/display/Booth.tsx
+++ src/components/display/Booth.tsx
@@ -1,11 +1,10 @@
-import { Text } from "@/components/typography/Text";
 import { TiHeartFullOutline } from "react-icons/ti";
+
 import { Variants } from "framer-motion";
-import { 	
-    BoothList,
-	BoothLink,
-	Index,
-	Heart } from "./Booth.styled";
+
+import { Text } from "@/components/typography/Text";
+
+import { BoothList, BoothLink, Index, Heart } from "./Booth.styled";
 
 interface IBooth {
     index: number;
@@ -13,8 +12,8 @@ interface IBooth {
     heart: number;
 }
 const itemVariants: Variants = {
-	hidden: { opacity: 0, y: 10 },
-	visible: { opacity: 1, y: 0 }
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0 },
 };
 
 export const Booth: React.FC<IBooth> = ({ index, name, heart }) => {
@@ -22,12 +21,14 @@ export const Booth: React.FC<IBooth> = ({ index, name, heart }) => {
         <BoothList variants={itemVariants}>
             <BoothLink href="#">
                 <Index>{index + 1}</Index>
-                <Text size="m" weight="bold" variant="#5D5A88">{name}</Text>
+                <Text size="m" weight="bold" variant="#5D5A88">
+                    {name}
+                </Text>
                 <Heart>
-                    <TiHeartFullOutline size={24}/>
+                    <TiHeartFullOutline size={24} />
                     {heart}
                 </Heart>
             </BoothLink>
         </BoothList>
     );
-}
+};
