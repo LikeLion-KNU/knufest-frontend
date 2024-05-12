f45aaf1aaf0384f4b14f9b8bb0683b7effffc983
junyeokk <cjh4302@gmail.com>
2024-05-12 11:04:58 +0900

    feat: 메인 페이지 카운트다운 컴포넌트 추가 (#5)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { Pagination } from "@/components/navigation/Pagination";
+import { Countdown } from "@/components/display/Countdown";
+import { Button } from "@/components/forms/Button";
+import { Text } from "@/components/typography/Text";
 
-import { PageContainer, MainContent, StyledImage } from "@/pages/HomePage.styled";
+import { PageContainer, MainContent, StyledImage, CountdownContainer } from "@/pages/HomePage.styled";
 
 import mainPageImage from "@/assets/main_page.jpg";
 
@@ -10,10 +12,28 @@ const HomePage: React.FC = () => {
     return (
         <PageContainer>
             <MainContent>
-                <StyledImage src={mainPageImage} alt="Main Page" />
-            </MainContent>
+                <StyledImage>
+                    <img src={mainPageImage} alt="Main Page" />
+                    <Button width="200px" variant="navy">
+                        <Text size="m" weight="bold" variant="white">
+                            시작하기
+                        </Text>
+                    </Button>
+                </StyledImage>
 
-            <Pagination />
+                {/* 카운트다운 */}
+                <CountdownContainer>
+                    <Text size="l" weight="bold" variant="darkpurple">
+                        대동제가 곧 시작됩니다
+                    </Text>
+                    <Countdown targetDate={new Date("2024-05-21T00:00:00")} />
+                </CountdownContainer>
+
+                {/* 빠른 시작 */}
+                <Text size="l" weight="bold" variant="darkpurple">
+                    축제 바로가기
+                </Text>
+            </MainContent>
         </PageContainer>
     );
 };
