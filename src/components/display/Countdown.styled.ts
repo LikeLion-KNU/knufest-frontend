d05a111a1100630e78510bf313d3acc7a621216a
junyeokk <cjh4302@gmail.com>
2024-05-12 11:01:54 +0900

    feat: 카운트다운 컴포넌트 구현 (#5)

---
+++ src/components/display/Countdown.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CountdownContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    font-family: "MesloLGS NF Bold", monospace;
+    font-size: 2rem;
+    color: #333;
+    text-align: center;
+    padding: 30px 60px;
+
+    span {
+        margin-bottom: 1rem;
+    }
+
+    border: 1px solid #d4d2e3;
+    border-radius: 24px;
+`;
