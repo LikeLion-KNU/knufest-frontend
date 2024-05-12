1e422e87196b052e66d910fa5a60f955990a6543
junyeokk <cjh4302@gmail.com>
2024-05-12 13:28:16 +0900

    feat: 홈페이지 공통 컨테이너 생성 및 스크롤 이동 구현 (#5)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
+import { ButtonList } from "@/components/display/ButtonList";
 import { Countdown } from "@/components/display/Countdown";
 import { Button } from "@/components/forms/Button";
 import { Text } from "@/components/typography/Text";
 
-import { PageContainer, MainContent, StyledImage, CountdownContainer } from "@/pages/HomePage.styled";
+import { PageContainer, MainContent, StyledImage, ElementContainer } from "@/pages/HomePage.styled";
 
 import mainPageImage from "@/assets/main_page.jpg";
 
 const HomePage: React.FC = () => {
+    const quickAccessRef = useRef<HTMLDivElement>(null);
+
+    const scrollToQuickAccess = () => {
+        quickAccessRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
     return (
         <PageContainer>
             <MainContent>
                 <StyledImage>
                     <img src={mainPageImage} alt="Main Page" />
-                    <Button width="200px" variant="navy">
+                    <Button width="200px" variant="navy" onClick={scrollToQuickAccess}>
                         <Text size="m" weight="bold" variant="white">
                             시작하기
                         </Text>
@@ -22,17 +29,20 @@ const HomePage: React.FC = () => {
                 </StyledImage>
 
                 {/* 카운트다운 */}
-                <CountdownContainer>
+                <ElementContainer>
                     <Text size="l" weight="bold" variant="darkpurple">
                         대동제가 곧 시작됩니다
                     </Text>
                     <Countdown targetDate={new Date("2024-05-21T00:00:00")} />
-                </CountdownContainer>
+                </ElementContainer>
 
                 {/* 빠른 시작 */}
-                <Text size="l" weight="bold" variant="darkpurple">
-                    축제 바로가기
-                </Text>
+                <ElementContainer ref={quickAccessRef}>
+                    <Text size="l" weight="bold" variant="darkpurple">
+                        축제 바로가기
+                    </Text>
+                    <ButtonList />
+                </ElementContainer>
             </MainContent>
         </PageContainer>
     );
