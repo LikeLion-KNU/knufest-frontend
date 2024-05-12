1a54e81c60f5a936fe6b44f8bacbba7bc19ac158
Hyoeunkh <lhe4528@gmail.com>
2024-05-12 14:45:35 +0900

    feat: 부스리스트 페이지 부스 추가

---
+++ src/pages/BoothListPage.styled.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const PageContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+`;
+
+export const VisibleList = styled(motion.ul)`
+	padding: 40px;
+	width: 90%;
+  list-style: none;
+
+	motion.IBooth {
+
+	}
+`;
--- src/pages/BoothListPage.tsx
+++ src/pages/BoothListPage.tsx
@@ -1,3 +1,47 @@
-export default function BoothListPage() {
-    return <></>;
-}
+import React from "react";
+import Navigation from "@/components/navigation/Navigation";
+import { Variants } from "framer-motion";
+// import Map from "./Map";
+import {
+	PageContainer,
+	MainContent,
+	VisibleList
+} from "./BoothListPage.styled";
+import { Booth } from "@/components/display/Booth";
+
+const listVariants: Variants = {
+	hidden: {
+		opacity: 0
+	},
+	visible: {
+		opacity: 1,
+		transition: {
+			when: "beforeChildren",
+			staggerChildren: 0.1
+		}
+	}
+};
+
+
+
+const BoothListPage: React.FC = () => {
+	const lists = ["컴퓨터학부 주점이름","전자 주점이름","토목 주점이름","기계 주점이름","식품공학부 주점이름"];
+	const heart = 356;
+    //list랑 index, heart는 받아온 값으로 수정
+
+  return (
+    <PageContainer>
+      <Navigation />
+      <MainContent>
+        {/*<Map />*/}
+        <VisibleList variants={listVariants} initial="hidden" animate="visible">
+					{lists.map((name,index) => {
+						return (
+						<Booth index={index} name={name} heart={heart}/>
+					)})}
+        </VisibleList>
+      </MainContent>
+    </PageContainer>
+  );
+};
+export default BoothListPage;
