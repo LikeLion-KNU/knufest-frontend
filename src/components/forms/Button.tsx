feb31c270eaecccd3ab7736a62509455e5ba7c69
junyeokk <cjh4302@gmail.com>
2024-05-12 11:05:15 +0900

    feat: 버튼 그림자 디자인 추가 (#5)

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -13,6 +13,7 @@ export const Button = styled.button<IButton>`
 
     border: none;
     border-radius: 40px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 
     color: #fff;
     background-color: ${(props) => {
