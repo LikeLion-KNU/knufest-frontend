e92c3c56a8941639c0db289b7bc1471f07baae12
junyeokk <cjh4302@gmail.com>
2024-05-12 20:55:03 +0900

    feat: 구글 애널리틱스 삽입

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig } from "vite";
+import { loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-});
+export default ({ mode }: { mode: string }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [
+            react(),
+            tsconfigPaths(),
+            createHtmlPlugin({
+                minify: true,
+                inject: {
+                    data: {
+                        google_analytics: env.VITE_GOOGLE_ANALYTICS,
+                    },
+                },
+            }),
+        ],
+    };
+};
