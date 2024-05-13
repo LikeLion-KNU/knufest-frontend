ca7c1aaca4ead6fc17eb6a5ba9a27e95bdcf724b
junyeokk <cjh4302@gmail.com>
2024-05-13 14:34:40 +0900

    feat: 만든이들 페이지 구현 (#9)

---
+++ src/pages/ContributorPage.styled.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const DefaultGap = styled.div`
+    margin-top: 120px;
+`;
+
+export const TextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+    text-align: center;
+    line-height: 1.5;
+
+    span {
+        margin-bottom: 10px;
+    }
+`;
+
+export const PositionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 40px 0;
+`;
+
+export const PositionTextWrapper = styled.div`
+    margin-bottom: 20px;
+    font-family: "MesloLGS NF", monospace;
+`;
