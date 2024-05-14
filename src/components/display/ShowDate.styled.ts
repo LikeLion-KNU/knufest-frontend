85b28488369fd70dd1ba8cb0d83ae2eaf9803c8d
Hyoeunkh <lhe4528@gmail.com>
2024-05-14 13:11:53 +0900

    feat: 공연 날짜 컴포넌트 구현

---
+++ src/components/display/ShowDate.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ShowDateContainer = styled.div`
+    justify-content: space-between;
+`;
+
+export const ShowDateLink = styled.a`
+    text-decoration: none;
+    text-align: center;
+    p {
+        margin: 0;
+        line-height: 22px;
+    }
+`;
+
+
