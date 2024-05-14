e9f1399163313a4e395e6726b2daeac45ae0702d
Hyoeunkh <lhe4528@gmail.com>
2024-05-14 13:10:25 +0900

    feat: paragraph 색 추가

--- src/components/typography/Paragraph.tsx
+++ src/components/typography/Paragraph.tsx
@@ -4,10 +4,10 @@ export interface IParagraph extends IText {
     children: React.ReactNode;
 }
 
-export const Paragraph: React.FC<IParagraph> = ({ size, weight, children }) => {
+export const Paragraph: React.FC<IParagraph> = ({ size, weight, variant, children }) => {
     return (
         <p>
-            <Text size={size} weight={weight}>
+            <Text size={size} weight={weight} variant={variant}>
                 {children}
             </Text>
         </p>
