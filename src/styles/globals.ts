cf2d6d8e2cfa6dacf959d02088df03a32956dce5
junyeokk <cjh4302@gmail.com>
2024-05-14 17:26:54 +0900

    fix: 폰트 pretendard로 설정

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
   @font-face {
     font-family: "MesloLGS NF Bold";
     src: url("./assets/MesloLGS-NF-Bold.ttf") format("truetype");
@@ -9,6 +11,6 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Arial', sans-serif;
+    font-family: 'Pretendard', sans-serif;
   }
 `;
