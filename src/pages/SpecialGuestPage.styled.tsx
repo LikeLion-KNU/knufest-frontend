dd72960da48a15e0dce2c3cd9a1e6f44c35c561f
junyeokk <cjh4302@gmail.com>
2024-05-15 21:06:22 +0900

    fix: speicla guest 페이지 데이터 변경

--- src/pages/SpecialGuestPage.styled.tsx
+++ src/pages/SpecialGuestPage.styled.tsx
@@ -5,7 +5,7 @@ export const MainContent = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-top: 20px;
+    margin-top: 50px;
     position: relative;
     p {
         text-shadow: 0px 2px 4px rgba(0, 0, 0, 25%);
@@ -27,7 +27,7 @@ export const GuestContainer = styled.div`
     flex-direction: column;
     gap: 36px;
     p {
-        text-shadow: none;
+        text-shadow: 0px 2px 4px rgba(0, 0, 0, 25%);
     }
 `;
 export const Date = styled.div`
