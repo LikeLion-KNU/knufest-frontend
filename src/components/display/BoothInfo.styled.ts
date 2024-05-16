898200ab51056cd2547a28e98ba0b4074c35531c
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-17 01:16:38 +0900

    feat: BoothInfo 구현

--- src/components/display/BoothInfo.styled.ts
+++ src/components/display/BoothInfo.styled.ts
@@ -1,19 +1,45 @@
 import styled from "styled-components";
 
 export const ImgWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
     overflow: hidden;
-    width: 100%;
-    position: relative;
+    width: 90%;
+    aspect-ratio: 1 / 1;
+    border-radius: 20px;
+    margin-bottom: 30px;
 `;
 
-export const BoothImg = styled.img``;
+export const BoothImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 50% 50%;
+`;
 
-export const InfoWrapper = styled.div``;
+export const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 26px;
+    width: 90%;
+`;
 
-export const BoothName = styled.div``;
+export const BoothName = styled.div`
+    background-color: #E7E6F2;
+    border-radius: 20px;
+    padding: 15px 20px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    flex-grow: 3;
+    div {
+        position: absolute;
+        right: 10px;
+    }
+`;
 
-export const BoothDetail = styled.div``;
+export const BoothDetail = styled.div`
+    padding: 0px 10px;
+    hr {
+        margin: 38px 0px;
+        border: 1px solid #dddddd;
+    }
+`;
--- src/components/display/BoothInfo.tsx
+++ src/components/display/BoothInfo.tsx
@@ -3,17 +3,32 @@ import React from "react";
 import mainPageImage from "@/assets/main_page.jpg";
 
 import { BoothDetail, BoothImg, BoothName, ImgWrapper, InfoWrapper } from "./BoothInfo.styled";
+import { Text } from "../typography/Text";
+
+import { Heart } from "./Heart";
 
 //zustand 로 refactor 필요
 const BoothInfo: React.FC = () => {
+    const boothdetail = "멋쟁이사자처럼 주점에서는 노래방 마이크로 김대건이 노래를 하고 춤도 추고 히어로 서사에 대해 설명할 것입니다.";
+    const boothName = "멋쟁이 사자처럼 주점";
+    const num = 365;
+
     return (
         <>
             <ImgWrapper>
                 <BoothImg src={mainPageImage} />
             </ImgWrapper>
             <InfoWrapper>
-                <BoothName>컴퓨터학부 포차 이름</BoothName>
-                <BoothDetail>컴퓨터학부 포차 정보</BoothDetail>
+                <BoothName>
+                    <Text size="20px" weight="bold" variant="#3F3A6C">{boothName}</Text>
+                    <Heart num={num} likable={true} />
+                </BoothName>
+                <BoothDetail>
+                    <Text size="s" weight="normal">
+                        {boothdetail}
+                    </Text>
+                    <hr />
+                </BoothDetail>
             </InfoWrapper>
         </>
     );
