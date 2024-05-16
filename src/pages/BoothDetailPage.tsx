9d28a3b6fb23090133f3aa7a5c03d1fb0eae953e
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-17 01:13:14 +0900

    feat: 부스 세부 페이지 구현

--- src/pages/BoothDetailPage.tsx
+++ src/pages/BoothDetailPage.tsx
@@ -1,14 +1,25 @@
 import BoothInfo from "@/components/display/BoothInfo";
 import CommentInfo from "@/components/display/CommentInfo";
+import sendImg from "@/assets/send.png";
 
-import { DetailPageWrapper } from "@/pages/BoothDetailPage.styled";
+import { DetailPageWrapper, SubBtn, ContentContainer, SendImg, BottomBox } from "@/pages/BoothDetailPage.styled";
 
 export default function BoothDetailPage() {
     return (
         <DetailPageWrapper>
             <BoothInfo />
-            <hr />
             <CommentInfo />
+            <BottomBox>
+                <ContentContainer>
+                    <textarea
+                        placeholder="댓글을 입력해주세요."
+                        rows={1}>
+                    </textarea>
+                    <SubBtn type="submit">
+                        <SendImg src={sendImg}/>
+                    </SubBtn>
+                </ContentContainer>
+            </BottomBox>
         </DetailPageWrapper>
     );
-}
+};
