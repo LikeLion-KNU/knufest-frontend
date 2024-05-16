43356b560d479ce640deda592e3a6f39905fbb99
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-17 01:17:27 +0900

    feat: CommentInfo 구현

--- src/components/display/CommentInfo.styled.ts
+++ src/components/display/CommentInfo.styled.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
-export const Title = styled.div``;
+export const CommentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    gap: 40px;
+`;
+export const Title = styled.div`
+    display: flex;
+    gap: 4px;
+    align-items: flex-end;
+`;
+export const CommentList = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    width: 95%;
+    div > hr {
+        margin-top: 38px;
+        border: 1px solid #dddddd;
+    }
+`;
 
-export const CommentList = styled.div``;
+export const CommentContainer = styled.div`
+`;
--- src/components/display/CommentInfo.tsx
+++ src/components/display/CommentInfo.tsx
@@ -2,43 +2,48 @@ import React, { useState } from "react";
 
 import { Pagination } from "@/components/navigation/Pagination";
 
-import CommentForm from "../forms/CommentForm";
 import Comment from "./Comment";
-import { CommentList, Title } from "./CommentInfo.styled";
+import { CommentList, CommentWrapper, CommentContainer, Title } from "./CommentInfo.styled";
+import { Text } from "../typography/Text";
 
-type Comment = {
+export interface IComment {
     name: string;
     time: string;
     content: string;
+    deletable: boolean;
 };
 
 const CommentInfo: React.FC = () => {
-    const [commentnum] = useState<number>(0);
+    const [commentnum] = useState<number>(221);
 
-    const comments: Comment[] = [
-        { name: "User1", time: "2024-05-13", content: "첫 번째 댓글입니다." },
-        { name: "User2", time: "2024-05-14", content: "두 번째 댓글입니다." },
-        { name: "User3", time: "2024-05-15", content: "세 번째 댓글입니다." },
+    const comments: IComment[] = [
+        { name: "User1", time: "2024-05-13", content: "첫 번째 댓글입니다.", deletable: false },
+        { name: "User2", time: "2024-05-14", content: "두 번째 댓글입니다.", deletable: true },
+        { name: "User3", time: "2024-05-15", content: "세 번째 댓글입니다.", deletable: false },
     ];
 
     return (
-        <>
-            <Title>
-                <div>댓글</div>
-                <div>{commentnum}</div>
-            </Title>
+        <CommentWrapper>
+            <Text size="20px" weight="bold">
+                <Title>
+                    댓글
+                    <Text size="m" weight="bold" variant="#3F3A6C">
+                        {commentnum}
+                    </Text>
+                </Title>
+            </Text>
             <CommentList>
                 {comments.map((comment, index) => (
                     <div>
-                        <Comment key={index} name={comment.name} time={comment.time} content={comment.content} />
+                        <CommentContainer>
+                            <Comment key={index} name={comment.name} time={comment.time} content={comment.content} deletable={comment.deletable} />
+                        </CommentContainer>
                         <hr />
                     </div>
                 ))}
             </CommentList>
             <Pagination />
-            <hr />
-            <CommentForm />
-        </>
+        </CommentWrapper>
     );
 };
 
