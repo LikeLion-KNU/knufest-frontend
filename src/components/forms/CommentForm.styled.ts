70d451b26391c964a1a1243638c47f7f80807801
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-17 01:19:42 +0900

    fix: 사용자명, 비밀번호 컴포넌트 제거

--- src/components/forms/CommentForm.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const InputId = styled.input``;
-
-export const InputPassword = styled.input``;
-
-export const InputContent = styled.input``;
-
-export const SubBtn = styled.button``;
