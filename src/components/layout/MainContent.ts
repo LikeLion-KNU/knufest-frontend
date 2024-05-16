c06b715f7e5eb9d2d7bfdaf9e4f76f1600321bff
junyeokk <cjh4302@gmail.com>
2024-05-17 01:45:12 +0900

    fix: MainContent flex 스타일 복구

--- src/components/layout/MainContent.ts
+++ src/components/layout/MainContent.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const MainContent = styled.div<{ isHomePage: boolean }>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     margin-top: ${({ isHomePage }) => (isHomePage ? "0px" : "50px")};
     position: relative;
 `;
