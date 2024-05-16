a62fad136a48264e23795035843006c3b0c7fb4d
Hyoeunkh <lhe4528@knu.ac.kr>
2024-05-17 01:12:49 +0900

    feat: Heart 컴포넌트 구현

--- src/pages/BoothListPage.tsx
+++ src/pages/BoothListPage.tsx
@@ -23,8 +23,8 @@ const listVariants: Variants = {
 };
 
 const BoothListPage: React.FC = () => {
-    const lists = ["컴퓨터학부 주점이름", "전자 주점이름", "토목 주점이름", "기계 주점이름", "식품공학부 주점이름"];
-    const heart = 356;
+    const lists = ["컴퓨터학부 주점이름","전자 주점이름", "토목 주점이름", "기계 주점이름", "식품공학부 주점이름"];
+    const num = 356;
     //list랑 index, heart는 받아온 값으로 수정
 
     const { visitorId } = useVisitor();
@@ -35,7 +35,7 @@ const BoothListPage: React.FC = () => {
                 {/*<Map />*/}
                 <VisibleList variants={listVariants} initial="hidden" animate="visible">
                     {lists.map((name, index) => {
-                        return <Booth index={index} name={name} heart={heart} />;
+                        return <Booth index={index} name={name} num={num} likable={true}/>;
                     })}
                 </VisibleList>
                 <p>{visitorId}</p>
